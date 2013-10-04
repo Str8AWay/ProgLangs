a84f5133d2d7d000707d6cb920242d00de6097f4
Jason Laqua <jason.laqua@mu.edu>
2013-10-03 19:55:33 -0500

    added if< functionality

--- Assignment3/assignment3.scm
+++ Assignment3/assignment3.scm
@@ -6,6 +6,7 @@
   [mult (lhs CWAE?) (rhs CWAE?)]
   [div (lhs CWAE?) (rhs CWAE?)]
   [minus (operand CWAE?)]
+  [if< (expr1 CWAE?) (expr2 CWAE?) (val1 CWAE?) (val2 CWAE?)]
   [with (name symbol?) (named-expr CWAE?) (body CWAE?)]
   [id (name symbol?)]
 )
@@ -29,16 +30,20 @@
                                      [#t (error 'parse "incorrect number of arguments for -")])]
         [(eq? (first sexp) 'with) (cond [(eq? 3 (length sexp))
                                             (cond [(list? (second sexp))
-                                                      (cond [(symbol? (first (second sexp)))
-                                                                (cond [(list? (third sexp))
-                                                                            (with (first (second sexp))
-                                                                                (parse (second (second sexp)))
-                                                                                (parse (third sexp)))]
-                                                                      [#t (error 'parse "third item in with expression isn't a list")])]
-                                                            [#t (error 'parse "no symbol for with expression")])]
+                                                      (cond [(eq? 2 (length (second sexp)))
+                                                                (cond [(symbol? (first (second sexp)))
+                                                                        (cond [(list? (third sexp))
+                                                                                (with (first (second sexp))
+                                                                                    (parse (second (second sexp)))
+                                                                                    (parse (third sexp)))]
+                                                                              [#t (error 'parse "third item in with expression isn't a list")])]
+                                                                      [#t (error 'parse "no symbol for with expression")])]
+                                                            [#t (error 'parse "second entry isn't a list")])]
                                                   [#t (error 'parse "second item in with expression isn't a list")])]
                                         [#t (error 'parse "incorrect number of arguments for with")])]
-        [else (error 'parse "unexpected expression")]
+        [(eq? (first sexp) 'if<) (cond [(eq? 5 (length sexp)) (if< (parse (second sexp)) (parse (third sexp)) (parse (fourth sexp)) (parse (fifth sexp)))]
+                                       [#t (error 'parse "incorrect number of arguments for if<")])]
+        [else (error (first sexp) "unexpected expression")]
   )
 )
 
@@ -59,6 +64,10 @@
     [div (l r) (div (subst l sub-id val)
                     (subst r sub-id val))]
     [minus (oper) (minus (subst oper sub-id val))]
+    [if< (expr1 expr2 val1 val2) (if< (subst expr1 sub-id val)
+                                      (subst expr2 sub-id val)
+                                      (subst val1 sub-id val)
+                                      (subst val2 sub-id val))]
     [with (bound-id named-expr bound-body)
           (if (symbol=? bound-id sub-id)
               (with bound-id
@@ -79,6 +88,8 @@
     [mult (l r) (* (calc l) (calc r))]
     [div (l r) (/ (calc l) (calc r))]
     [minus (oper) (- (calc oper))]
+    [if< (expr1 expr2 val1 val2) (cond [(< (calc expr1) (calc expr2)) (calc val1)]
+                                       [#t (calc val2)])]
     [with (s nexpr body) (calc (subst body s (num (calc nexpr))))]
     [id (s) (error 'calc "unexpected free variable encountered")]
   )
